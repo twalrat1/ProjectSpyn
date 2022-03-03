78651f877c50061536904bb60f9634f5b4b43896
twalrat1 <100380011+twalrat1@users.noreply.github.com>
2022-03-02 20:37:56 -0700

    Delete PlayTone.m

--- PlayTone.m
+++
@@ -1,5 +0,0 @@
-% Example 1: Play Tone and Get Battery Level
-% Play tone with frequency 800Hz and duration of 500ms.
-brick.playTone(100, 800, 500);
-% Get current battery level.
-v = brick.GetBattVoltage();
