68f0c6664a7eaea17d18071e949e05519e406a5d
hollyangle <hollyangleroblox@gmail.com>
2022-04-13 10:22:27 -0700

    Update MapPrototype.m

--- MapPrototype.m
+++ MapPrototype.m
@@ -1,11 +1,23 @@
 %temp values for testing, distance will be fed through D1,D2,D3,D4 where 1,2,3,4 designate the cardinal direction
-posx = 0;
-x1 = 0;
-y1 = 0;
+posCheck = 0;
 dist = 100;
-pos = [x1,y1];
+dir = 4;
+pos = [0,0];
 
-%initalizing the map, 1000x1200 is equivalent to 4 5x6 piecedd together
+% generateDir(dist, pos)
+createMap()
+if posCheck == 0
+    [pos, posCheck] = setStart();
+else
+    findpos()
+end
+%while running
+% createColorSquare();
+updateData(dist, pos, dir)
+%end
+
+%initalizing the map, 1000x1200 is equivalent to four 5x6 pieced together
+function createMap()
 hold on
 
 xlim([0,1000]);
@@ -13,92 +25,80 @@ ylim([0,1200]);
 p = plot(xlim,ylim,'linestyle','none');
 axis equal;
 grid on
-
-generateDir(dist, pos)
-findPos(posx)
-
-function findPos(posx, pos)
-    switch posx
-        case 0
-            setStart(pos)
-            posx = 1;
-        case 1
-    end
 end
 
-function setStart(pos)
-%generate 4 lines at center of plot for visual data, setPos to one data point for computation
+function findPos()
+    rectangle('Position',[pos(1)+25 pos(2)+25 25 25]);
+%in progress
+    %pos = [pos(1) + dist, pos(2) + dist]
+%In main code, set dir to 1-4 during the distancing reading
+    %updateData(dist, pos, dir)
+    refreshdata
+    drawnow
 end
 
-function generateDir(dist, pos) %placeholder code to generate random #s for testing 4 different directions 
-        a = 1;
-        b = 4;
-        count = 0;
+% function createColorSquare()
+%     rectangle('Position',[pos(1)+25 pos(2)+25 25 25]);
+%     refreshdata
+%     drawnow
+% end
 
-        dir = a + (b-a).*rand(1);
-        dir = round(dir,0);
-        disp(dir);
-
-        count = count + 1;
-
-    updateData(dist, pos, dir, count);
+function [pos, posCheck] = setStart()
+    pos = [400,600];
+    posCheck = 1;
+    rectangle('Position',[pos(1)+37 pos(2)+30 25 25]);
+    refreshdata
+    drawnow
 end
 
-function updateData(dist, pos, dir, count) %Update the map/figure with a new wall based off incoming distance reading
+function updateData(dist, pos, dir) %Update the map/figure with a new wall based off incoming distance reading
 
     p.XDataSource = 'x2';
     p.YDataSource = 'y2';
 
-while count >= 1
-    %Dir should be 1,2,3,4 for north, south, east, or west
+    %Direction should be 1,2,3,4 for north, south, east, or west
   switch dir
     case 1
-        x2 = pos + [dist, 0];
-        y2 = pos + [dist,dist];
+        x2 = [pos(1), pos(1) + dist];
+        y2 = [pos(2)+dist, pos(2) + dist];
         line(x2,y2,'linestyle','-','Color','r');
         refreshdata
         drawnow
     
-        disp("north")
-        count = count - 1;
+        %disp("north")
 
     case 2
-        x2 = pos + [0, dist];
-        y2 = pos + [0,0];
-        line(x2,y2,'linestyle','-','Color','b');
+        x2 = [pos(1), pos(1) + dist];
+        y2 = [pos(2), pos(2)];
+        line(x2,y2,'linestyle','-','Color','r');
         refreshdata
         drawnow
     
-        disp("south")
-        count = count - 1;
+        %disp("south")
 
     case 3
-        x2 = pos + [dist, dist];
-        y2 = pos + [dist,0];
-        line(x2,y2,'linestyle','-','Color','g');
+        x2 = [pos(1), pos(1)];
+        y2 = [pos(2), pos(2)+dist];
+        line(x2,y2,'linestyle','-','Color','r');
         refreshdata
         drawnow
     
-        disp("east")
-        count = count - 1;
+        %disp("east")
 
     case 4
-        x2 = pos + [0, 0];
-        y2 = pos + [0,dist];
-        line(x2,y2,'linestyle','-','Color','y');
+        x2 = [pos(1) + dist, pos(1) + dist];
+        y2 = [pos(2), pos(2) + dist];
+        line(x2,y2,'linestyle','-','Color','r');
         refreshdata
         drawnow
     
-        disp("west")
-        count = count - 1;    
+        %disp("west")    
 
     case 0
         disp("Fail")
 
-        count = count - 1;
   end
 
     refreshdata
     drawnow
-end
 end
